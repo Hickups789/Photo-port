e81d59024b50797f298d0657f44a1d3c89118e0c
Hickups789 <jackysantiago358@yahoo.com>
2022-10-20 19:10:46 -0400

    contact

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -33,13 +33,17 @@ describe('Nav component', () => {
     { name: 'portraits', description: 'Portraits of people in my life' }
   ]
   const mockCurrentCategory = jest.fn();
-  const mockSetCurrentCategory = jest.fn();  
+  const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();  
 
   it('renders', () => {
     render(<Nav
       categories={categories}
       setCurrentCategory={mockSetCurrentCategory}
       currentCategory={mockCurrentCategory}
+      ContactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
     />);
   })
    
